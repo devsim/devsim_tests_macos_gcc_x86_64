56db92a261163ca59bdc31fce70429d70d241ecc
Juan E. Sanchez <juan@tcad.com>
2018-10-22 21:52:40 -0500

    preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -2,9 +2,10 @@
 set -e
 
 if [ ! -d ${HOME}/anaconda ]; then
-( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh  && bash Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
-${HOME}/anaconda/bin/conda create -y --name python3 python=3
-${HOME}/anaconda/bin/conda install -y numpy mkl
-${HOME}/anaconda/bin/conda create -y --name python3 python=3
-${HOME}/anaconda/bin/conda install -y -n python3 numpy mkl
+( cd ${HOME} && curl -O https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh  && bash Miniconda2-latest-Linux-x86_64.sh -b -p ${HOME}/anaconda )
+
+${HOME}/anaconda/bin/conda create  -y --name python27_devsim python=2.7
+${HOME}/anaconda/bin/conda install -y --name python27_devsim numpy mkl
+${HOME}/anaconda/bin/conda create  -y --name python37_devsim python=3.7
+${HOME}/anaconda/bin/conda install -y --name python27_devsim numpy mkl
 fi
