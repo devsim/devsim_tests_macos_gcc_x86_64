d225eac4e283e1d1ea37b4edab8a506696c39764
Juan E. Sanchez <juan@tcad.com>
2019-05-17 20:44:13 -0500

    stop using symlink to testing and examples

--- travis_tests.sh
+++ travis_tests.sh
@@ -32,26 +32,8 @@ export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
 python "\$@"
 EOF
 chmod +x bin/devsim_py37
-
-cat << EOF > bin/devsim_tcl
-#!/bin/bash
-set -e
-progname="\$0"
-curdir=\`dirname "\${progname}"\`
-ANACONDA_PATH=${ANACONDA_PATH}
-source \${ANACONDA_PATH}/bin/activate python37_devsim
-export DYLD_LIBRARY_PATH=\${CONDA_PREFIX}/lib
-export TCL_LIBRARY=\${CONDA_PREFIX}/lib/tcl8.6
-# sequential speeds up small examples
-export MKL_NUM_THREADS=1
-\${curdir}/../${DEVSIM_TCL} "\$@"
-EOF
-chmod +x bin/devsim_tcl
-
-ln -sf ${TAGDIR}/testing .
-ln -sf ${TAGDIR}/examples .
-
+cp CMakeLists.txt ${TAGDIR}/
 rm -rf run && mkdir run
-(cd run && ../bin/cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 -DDEVSIM_TCL_TEST_EXE=${BASEDIR}/bin/devsim_tcl ..)
+(cd run && ../bin/cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 ../${TAGDIR})
 (cd run && (../bin/ctest -j4 --no-compress-output -T Test || true))
 
