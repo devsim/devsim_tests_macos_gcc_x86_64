dd49233a1752f041e3848997624cb52f5fddf8e3
Juan E. Sanchez <juan@tcad.com>
2019-05-17 20:52:02 -0500

    adjust script

--- travis_tests.sh
+++ travis_tests.sh
@@ -12,7 +12,6 @@ ANACONDA_PATH=${HOME}/anaconda
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
-#UTILITY_PATH=$(source ${ANACONDA_PATH}/bin/activate python37_devsim && echo ${CONDA_PREFIX}/bin)
 UTILITY_PATH=${ANACONDA_PATH}/bin/
 
 mkdir -p bin
@@ -24,10 +23,10 @@ set -e
 progname="\$0"
 curdir=\`dirname "\${progname}"\`
 ANACONDA_PATH=${ANACONDA_PATH}
-source \${ANACONDA_PATH}/bin/activate python37_devsim
 export PYTHONHASHSEED=0
 # sequential speeds up small examples
 export MKL_NUM_THREADS=1
+source \${ANACONDA_PATH}/bin/activate python37_devsim
 export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
 python "\$@"
 EOF
