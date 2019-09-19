6099dbe0766840503225db10c21899eb6b815abd
Juan E. Sanchez <juan@tcad.com>
2019-09-18 23:27:38 -0500

    update script

--- travis_tests.sh
+++ travis_tests.sh
@@ -4,35 +4,25 @@ BASEDIR="${PWD}"
 TAG=${1}
 TAGDIR=devsim_macos_${TAG}
 TAGTGZ=${TAGDIR}.tgz
-#DEVSIM_PY=${TAGDIR}/bin/devsim
-#DEVSIM_PY3=${TAGDIR}/bin/devsim_py3
-DEVSIM_TCL=${TAGDIR}/bin/devsim_tcl
 DEVSIM_LIB=${TAGDIR}/lib
-ANACONDA_PATH=${HOME}/anaconda
 
 #curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
 #tar xzf ${TAGTGZ} 
-UTILITY_PATH=${ANACONDA_PATH}/bin/
-
-mkdir -p bin
-(cd bin && ln -sf ${UTILITY_PATH}/cmake && ln -sf ${UTILITY_PATH}/ctest)
 
 cat << EOF > bin/devsim_py37
 #!/bin/bash
 set -e
 progname="\$0"
 curdir=\`dirname "\${progname}"\`
-ANACONDA_PATH=${ANACONDA_PATH}
 export PYTHONHASHSEED=0
 # sequential speeds up small examples
 export MKL_NUM_THREADS=1
-source \${ANACONDA_PATH}/bin/activate python37_devsim
 export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
 python "\$@"
 EOF
 chmod +x bin/devsim_py37
 cp CMakeLists.txt ${TAGDIR}/
 rm -rf run && mkdir run
-(cd run && ../bin/cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 ../${TAGDIR})
-(cd run && (../bin/ctest -j4 --no-compress-output -T Test || true))
+(cd run && cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py37 ../${TAGDIR})
+(cd run && (ctest -j4 --no-compress-output -T Test || true))
 
