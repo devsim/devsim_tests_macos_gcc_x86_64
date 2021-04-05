4b7f3f22f7fc9efa820f2a94b0d84160024418ef
Juan E. Sanchez <juan@tcad.com>
2021-04-04 22:59:25 -0500

    update

--- preinstall.sh
+++ preinstall.sh
@@ -5,8 +5,7 @@ if [ ! -d ${HOME}/anaconda ]; then
 ( cd ${HOME} && curl -L -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh && bash Miniconda3-latest-MacOSX-x86_64.sh -b -p ${HOME}/anaconda )
 fi
 
-${HOME}/anaconda/bin/conda install -y --name base cmake
-${HOME}/anaconda/bin/conda create  -y --name python37_devsim python=3.7
+${HOME}/anaconda/bin/conda create  -y --name python38_devsim python=3.8
 # mkl is implicit
-${HOME}/anaconda/bin/conda install -y --name python37_devsim numpy
+${HOME}/anaconda/bin/conda install -y --name python38_devsim numpy mkl cmake
 
