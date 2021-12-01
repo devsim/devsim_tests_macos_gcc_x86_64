acc78e2c2e1e464431b6a3264ccfb4f0fd0ec7da
Juan E. Sanchez <juan@tcad.com>
2021-11-30 21:58:21 -0600

    update

--- preinstall.sh
+++ preinstall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-set -e
+#set -e
 
-if [ ! -d ${HOME}/anaconda ]; then
-( cd ${HOME} && curl -L -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh && bash Miniconda3-latest-MacOSX-x86_64.sh -b -p ${HOME}/anaconda )
-fi
-
-${HOME}/anaconda/bin/conda create  -y --name python38_devsim python=3.8
-# mkl is not implicit to mkl see: https://mruss.dev/numpy-mkl/
-${HOME}/anaconda/bin/conda install -y --name python38_devsim numpy mkl cmake blas=*=*mkl
+conda install -y --name base cmake
+conda create  -y --name devsim_test python=3 numpy mkl cmake
+conda activate devsim_test
 
