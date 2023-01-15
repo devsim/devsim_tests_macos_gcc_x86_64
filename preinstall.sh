b7ddf37415a6776f556e519a1f6a2213c224d1b7
Juan E. Sanchez <juan@tcad.com>
2023-01-15 12:58:09 -0600

    prepare for release

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,6 @@
 #set -e
 
 #conda install -y --name base cmake
-conda create  -y --name devsim_test python=3 numpy mkl cmake blas=*=*mkl
+conda create  -y --name devsim_test python=3 numpy mkl cmake "blas=*=*mkl"
 conda activate devsim_test
 
